e6f59cca42acb31de1818877dbe16e2c553c9d80
Alex Galvis <alex.galvis.sistemas@gmail.com>
2022-08-15 21:18:29 -0500

    Fix runtime error

---
+++ src/main/java/com/gsdd/wikipedia/page/object/WikipediaPageObject.java
@@ -0,0 +1,30 @@
+package com.gsdd.wikipedia.page.object;
+
+import lombok.Getter;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.questions.Text;
+import net.serenitybdd.screenplay.targets.Target;
+import net.serenitybdd.screenplay.ui.InputField;
+import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.Keys;
+
+@Getter
+@DefaultUrl("http://wikipedia.com/")
+public class WikipediaPageObject extends PageObject {
+  
+  public static final Target SEARCH = InputField.withNameOrId("search");
+  
+  public Performable searchByKeyword(String keyword) {
+    return Task.where("{0} searches for articles containing they word " + keyword,
+        Enter.theValue(keyword).into(InputField.withNameOrId("search")).thenHit(Keys.ENTER));
+  }
+  
+  public Question<String> firstHeading() {
+    return Text.of("#firstHeading");
+  }
+
+}
--- src/test/java/com/gsdd/runners/WikipediaScreenPlayTest.java
+++ src/test/java/com/gsdd/runners/WikipediaScreenPlayTest.java
@@ -5,20 +5,16 @@ import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.GivenWhenThen.then;
 import static net.serenitybdd.screenplay.GivenWhenThen.when;
 import static org.hamcrest.CoreMatchers.containsString;
+import com.gsdd.wikipedia.page.object.WikipediaPageObject;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.Performable;
-import net.serenitybdd.screenplay.Question;
-import net.serenitybdd.screenplay.Task;
-import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.annotations.CastMember;
-import net.serenitybdd.screenplay.questions.Text;
-import net.serenitybdd.screenplay.ui.InputField;
 import net.thucydides.core.annotations.Managed;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 
@@ -30,25 +26,18 @@ class WikipediaScreenPlayTest {
   private WebDriver browser;
   @CastMember(name = "foxy")
   private Actor foxy;
-  
-  private Performable navigateToTheHomePage() {
-    return Open.url("http://wikipedia.com/");
-  }
-  
-  private Performable searchByKeyword(String keyword) {
-    return Task.where("{0} searches for articles containing they word " + keyword,
-        Enter.theValue(keyword).into(InputField.withNameOrId("search")).thenHit(Keys.ENTER));
-  }
-  
-  private Question<String> firstHeading() {
-    return Text.of("#firstHeading");
+  private WikipediaPageObject pageObject = new WikipediaPageObject();
+
+  @BeforeEach
+  public void setup() {
+    foxy.can(BrowseTheWeb.with(browser));
   }
 
   @Test
   void searchByKeyword() {
-    givenThat(foxy).wasAbleTo(navigateToTheHomePage());
-    when(foxy).attemptsTo(searchByKeyword(TEXT));
-    then(foxy).should(seeThat(firstHeading(), containsString("Mount Everest")));
+    givenThat(foxy).wasAbleTo(Open.browserOn(pageObject));
+    when(foxy).attemptsTo(pageObject.searchByKeyword(TEXT));
+    then(foxy).should(seeThat(pageObject.firstHeading(), containsString("Mount Everest")));
   }
 
 }
